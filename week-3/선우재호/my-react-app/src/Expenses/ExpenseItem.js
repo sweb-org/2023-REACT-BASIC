077884e7d61f28ccc9290a99fbe35804de34e12f
SunwooJaeho <118053865+SunwooJaeho@users.noreply.github.com>
2023-04-07 14:36:39 +0900

    [선우재호] Week-3 complete

---
+++ week-3/선우재호/my-react-app/src/Expenses/ExpenseItem.js
@@ -0,0 +1,22 @@
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
+
+function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log("Clicked!!");
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Click Me</button>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
